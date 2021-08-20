b1cc78daa42a3b3442e79850a3b63477db8e170a
TEMOH <adesojidaniel139@yahoo.com>
2021-08-20 12:54:30 +0100

    First Push

---
+++ print.js
@@ -0,0 +1,6 @@
+//PROGRAM TO PRINT OUT NAME IN CONSOLE
+
+// TO run this file using node, after installing node, Run this file in your terminal
+// Using node print.js
+
+console.log('NAME : ADESOJI DANIEL TEMILOLUWA');
